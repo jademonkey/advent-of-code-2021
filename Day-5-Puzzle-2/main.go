fa7dcc088b0209f4bf144a3258f6a6fecfc477f3
Rob Parker <jademonkey7@gmail.com>
2021-12-05 22:54:37 +0000

    Day 5!

---
+++ Day-5-Puzzle-2/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Coords struct {
+	StartX int
+	StartY int
+	EndX   int
+	EndY   int
+}
+
+func main() {
+	log.Println(">Grabbing input")
+	allCoords, maxX, maxY, err := ReadLineCoords("input")
+	if err != nil {
+		log.Printf("Got error: %v\n", err)
+		os.Exit(1)
+	}
+	log.Println(">Running solution")
+	answer, err := calcSolution(allCoords, maxX, maxY)
+	if err != nil {
+		log.Printf("Got error: %v\n", err)
+		os.Exit(2)
+	}
+	log.Printf("Answer: %v\n", answer)
+}
+
+func calcSolution(allCoords []Coords, maxX, maxY int) (int, error) {
+	if allCoords == nil {
+		return 0, fmt.Errorf("allCoords array was nil")
+	}
+
+	var Board [][]int
+	var solution int
+
+	for i := 0; i < maxY+1; i++ {
+		// array using make method
+		arrayWithMake := make([]int, (maxX + 1))
+		Board = append(Board, arrayWithMake)
+	}
+
+	for _, cor := range allCoords {
+		var myStart, myEnd int
+		if cor.StartX == cor.EndX {
+			// Column
+			if cor.StartY < cor.EndY {
+				myStart = cor.StartY
+				myEnd = cor.EndY
+			} else if cor.EndY < cor.StartY {
+				myStart = cor.EndY
+				myEnd = cor.StartY
+			} else {
+				myStart = cor.StartY
+				myEnd = cor.StartY
+			}
+			for c := myStart; c < (myEnd + 1); c++ {
+				Board[cor.StartX][c]++
+			}
+		} else if cor.StartY == cor.EndY {
+			// Row
+			if cor.StartX < cor.EndX {
+				myStart = cor.StartX
+				myEnd = cor.EndX
+			} else if cor.EndX < cor.StartX {
+				myStart = cor.EndX
+				myEnd = cor.StartX
+			} else {
+				myStart = cor.StartX
+				myEnd = cor.StartX
+			}
+			for r := myStart; r < (myEnd + 1); r++ {
+				Board[r][cor.StartY]++
+			}
+		} else {
+			//Diagonal
+			myStartX := cor.StartX
+			myStartY := cor.StartY
+
+			for {
+				Board[myStartX][myStartY]++
+				if myStartX > cor.EndX {
+					myStartX--
+				} else if myStartX < cor.EndX {
+					myStartX++
+				} else {
+					// end
+					break
+				}
+				if myStartY > cor.EndY {
+					myStartY--
+				} else if myStartY < cor.EndY {
+					myStartY++
+				} else {
+					// end
+					break
+				}
+			}
+
+		}
+
+	}
+	// Now we iterate through
+	for r := 0; r < maxX+1; r++ {
+		for c := 0; c < maxY+1; c++ {
+			if Board[r][c] > 1 {
+				solution++
+			}
+		}
+	}
+
+	return solution, nil
+}
+
+func ReadLineCoords(filename string) ([]Coords, int, int, error) {
+	var finalCoords []Coords
+	var err error
+	var largestX, largestY int
+	fileH, err := os.Open(filename)
+	if err != nil {
+		return nil, 0, 0, nil
+	}
+	defer fileH.Close()
+	log.Printf("Opened Input file\n")
+
+	fileReader := bufio.NewReader(fileH)
+	for {
+		var line string
+		line, err = fileReader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				break
+			}
+			log.Printf("Failed to read line %v\n", err)
+			break
+		}
+		line = strings.Trim(line, "\n\r ")
+		line = strings.ReplaceAll(line, " -> ", ",")
+		if len(line) == 0 {
+			// skip blank lines
+			continue
+		}
+
+		numbers := strings.Split(line, ",")
+		if len(numbers) != 4 {
+			err = fmt.Errorf("array of numbers was wrong")
+			log.Printf("%v\n", err)
+			break
+		}
+
+		startX, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			log.Printf("Failed to convert int %v - %v\n", numbers[0], err)
+			break
+		}
+
+		if startX > largestX {
+			largestX = startX
+		}
+
+		startY, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			log.Printf("Failed to convert int %v - %v\n", numbers[1], err)
+			break
+		}
+
+		if startY > largestY {
+			largestY = startY
+		}
+
+		endX, err := strconv.Atoi(numbers[2])
+		if err != nil {
+			log.Printf("Failed to convert int %v - %v\n", numbers[2], err)
+			break
+		}
+
+		if endX > largestX {
+			largestX = endX
+		}
+
+		endY, err := strconv.Atoi(numbers[3])
+		if err != nil {
+			log.Printf("Failed to convert int %v - %v\n", numbers[3], err)
+			break
+		}
+
+		if endY > largestY {
+			largestY = endY
+		}
+
+		coord := Coords{startX, startY, endX, endY}
+		finalCoords = append(finalCoords, coord)
+	}
+
+	return finalCoords, largestX, largestY, nil
+}
---
+++ Day-5-Puzzle-2/main_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestReadLineCoords(t *testing.T) {
+	Input1 := Coords{0, 9, 5, 9}
+	Input2 := Coords{8, 0, 0, 8}
+	Input3 := Coords{9, 4, 3, 4}
+	Input4 := Coords{2, 2, 2, 1}
+	Input5 := Coords{7, 0, 7, 4}
+	Input6 := Coords{6, 4, 2, 0}
+	Input7 := Coords{0, 9, 2, 9}
+	Input8 := Coords{3, 4, 1, 4}
+	Input9 := Coords{0, 0, 8, 8}
+	Input0 := Coords{5, 5, 8, 2}
+
+	CoordsArray := []Coords{Input1, Input2, Input3, Input4, Input5, Input6, Input7, Input8, Input9, Input0}
+	MaxX := 9
+	MaxY := 9
+
+	MyArray, X, Y, err := ReadLineCoords("testInput")
+	if err != nil {
+		t.Fatalf("ReadLineCoords error'd: %v", err)
+	}
+
+	if MaxX != X {
+		t.Errorf("ReadLineCoords MaxX does not match.\nExpected: %v\n     Got: %v", MaxX, X)
+	}
+
+	if MaxY != X {
+		t.Errorf("ReadLineCoords MaxY does not match.\nExpected: %v\n     Got: %v", MaxY, Y)
+	}
+
+	if !CompareCoordsArray(CoordsArray, MyArray) {
+		t.Errorf("ReadLineCoords CoordArray does not match.\nExpected: %v\n     Got: %v", CoordsArray, MyArray)
+	}
+
+}
+
+func CompareCoordsArray(one, two []Coords) bool {
+	if one == nil || two == nil {
+		return false
+	}
+	if len(one) != len(two) {
+		return false
+	}
+
+	for i := 0; i < len(one); i++ {
+		if one[i].StartX != two[i].StartX {
+			return false
+		}
+		if one[i].StartY != two[i].StartY {
+			return false
+		}
+		if one[i].EndX != two[i].EndX {
+			return false
+		}
+		if one[i].EndY != two[i].EndY {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestCalculateSolution(t *testing.T) {
+	Input1 := Coords{0, 9, 5, 9}
+	Input2 := Coords{8, 0, 0, 8}
+	Input3 := Coords{9, 4, 3, 4}
+	Input4 := Coords{2, 2, 2, 1}
+	Input5 := Coords{7, 0, 7, 4}
+	Input6 := Coords{6, 4, 2, 0}
+	Input7 := Coords{0, 9, 2, 9}
+	Input8 := Coords{3, 4, 1, 4}
+	Input9 := Coords{0, 0, 8, 8}
+	Input0 := Coords{5, 5, 8, 2}
+
+	CoordsArray := []Coords{Input1, Input2, Input3, Input4, Input5, Input6, Input7, Input8, Input9, Input0}
+	MaxX := 9
+	MaxY := 9
+
+	expectedAnswer := 12
+
+	answer, err := calcSolution(CoordsArray, MaxX, MaxY)
+	if err != nil {
+		t.Fatalf("calcSolution error'd: %v", err)
+	}
+
+	if answer != expectedAnswer {
+		t.Errorf("calcSolution answer does not match.\nExpected: %v\n     Got: %v", expectedAnswer, answer)
+	}
+}
